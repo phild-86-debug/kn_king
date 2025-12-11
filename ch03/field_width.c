54e71cef4f304b67a2ce64a645306b630b51ebf0
Phil Dexter <phildexter86@gmail.com>
2025-12-11 09:38:21 -0500

    Added field_width.c

---
+++ ch03/field_width.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+
+    int i = 12345;
+    printf("int i: %10d\n", i);
+    printf("int i: %-10d", i);
+    printf("%d\n", 123345);
+    return 0;
+
+}
+
+
