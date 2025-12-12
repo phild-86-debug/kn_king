2119d36f66d123c68bd5f169e2fd86260a47c72e
Phil Dexter <phildexter86@gmail.com>
2025-12-12 11:17:42 -0500

    Added project5.c

---
+++ ch03/projects/project5.c
@@ -0,0 +1,21 @@
+/*
+ * program5.c                                           *
+ * 12/12/2025                                           *
+ * Write a program that asks the user to enter the      *
+ * numbers from 1-16. 1) display the numbers in a       *
+ * 4x4 arrangement. 2) print sum of row, columns        *
+ * and diagnal.                                         *
+
+*/
+
+
+#include <stdio.h>
+
+
+int main(void)
+{
+    int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12,n13,n14,n15,n16;
+    printf("Enter the numbers from 1-16 in any order: ");
+    scanf("%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d", &n1, &n2, &n3, &n4, &n5, &n6, &n7, &n8, &n9, &n10, &n11, &n12, &n13, &n14, &n15, &n16);
+    
+}
