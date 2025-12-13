22d06fdebe296fe965a38059e8973d0f33004da8
Phil Dexter <phildexter86@gmail.com>
2025-12-13 14:49:30 -0500

    Added project3.c

---
+++ ch04/projects/project3.c
@@ -0,0 +1,17 @@
+/*
+ * project3.c
+ * Reverse Digits(without using math)
+ * input: Three digit number
+ * output: three digit number with digits reversed
+ * Date: 12/13/2025
+ * 
+*/
+
+
+#include <stdio.h>
+
+
+int main(void)
+{
+    
+}
