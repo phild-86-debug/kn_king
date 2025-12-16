7f922af216373c8b227829cd8c5783040b2c6df1
Phil Dexter <phildexter86@gmail.com>
2025-12-16 14:24:53 -0500

    Added project9.c

---
+++ ch05/projects/project9.c
@@ -0,0 +1,41 @@
+/* 
+ * project9.c
+ * earliest date
+ * 12/16/2025
+ * input:  two dates
+ * output: The date which comes earlier
+
+ */
+
+
+#include <stdio.h>
+
+int main(void)
+{
+    int m1, d1, y1;
+    int m2, d2, y2;
+
+    printf("Enter first date in the format (mm/dd/yy): ");
+    scanf("%d/%d/%d", &m1, &d1, &y1);
+    printf("Enter second date in the format (mm/dd/yy): ");
+    scanf("%d/%d/%d", &m2, &d2, &y2);
+
+    if(y1 < y2)
+        printf("%d/%d/%.2d is earlier than %d/%d/%.2d\n", m1, d1, y1, m2, d2, y2);
+    else if(y2 < y1)
+        printf("%d/%d/%.2d is earlier than %d/%d/%.2d\n", m2, d2, y2, m1, d1, y1);
+    else if(m1 < m2)
+        printf("%d/%d/%.2d is earlier than %d/%d/%.2d\n", m1, d1, y1, m2, d2, y2);
+    else if(m2 < m1)
+        printf("%d/%d/%.2d is earlier than %d/%d/%.2d\n", m2, d2, y2, m1, d1, y1);    
+    else if(d1 < d2)
+        printf("%d/%d/%.2d is earlier than %d/%d/%.2d\n", m1, d1, y1, m2, d2, y2);
+    else if(d2 < d1)
+        printf("%d/%d/%.2d is earlier than %d/%d/%.2d\n", m2, d2, y2, m1, d1, y1);
+    else
+        printf("The dates are equal\n");
+        
+
+    return 0;
+
+}
