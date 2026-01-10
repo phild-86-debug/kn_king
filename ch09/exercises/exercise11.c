bbe4669d1ec33540d3fb334706695559fe28ac01
Phil Dexter <phildexter86@gmail.com>
2026-01-10 11:45:05 -0500

    Added exercise11.c

---
+++ ch09/exercises/exercise11.c
@@ -0,0 +1,58 @@
+/* exercise11.c
+ * 01/10/2026
+ * write a function float compute_GPA(char grades[], int n).
+ * The function computes a gpa from letter grades to a numerical
+ * average and returns the average. 
+ */
+
+
+ #include <stdio.h>
+ #include <ctype.h>
+
+float compute_GPA(char grades[], int n);
+int main(void){
+    int n;
+    char ch;
+    printf("Enter size of the array: ");
+    scanf("%d", &n);
+    char grades[n];
+    printf("Enter %d grades: ", n);
+    for (int i = 0; i < n; i++){
+        scanf(" %c", &ch);
+        grades[i] = ch;
+    }
+    
+
+    printf("GPA: %.2f\n", compute_GPA(grades, n));
+
+    return 0;
+}
+
+
+float compute_GPA(char grades[], int n){
+    int average, grade_total = 0;
+    for (int i = 0; i < n; i++){
+        grades[i] = toupper(grades[i]);
+        switch(grades[i])
+        {
+            case 'A':
+                grade_total += 4;
+                break;
+            case 'B':
+                grade_total += 3;
+                break;
+            case 'C':
+                grade_total += 2;
+                break;
+            case 'D':
+                grade_total += 1;
+                break;
+            case 'F':
+                grade_total += 0;
+                break;
+        }
+    }
+    return (float)grade_total / n;
+
+
+}
